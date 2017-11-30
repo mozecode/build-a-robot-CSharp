1605b9c0ba137182d8df1235001cab67dced2511
Greta Moseley <greta.moseley@gmail.com>
2017-11-30 15:49:54 -0600

    "add main program file for robot builder exercise"

---
+++ ExamPractice2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Robot Ordering App!");
+
+            Console.WriteLine();//blank
+
+            Console.Write("How many robots?  >> ");
+            int quantity;
+            int arms;
+            string transmode;
+
+            while (int.TryParse(Console.ReadLine(), out quantity)==false || quantity<0)
+            { Console.Write("Try Again: "); }
+
+  
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
